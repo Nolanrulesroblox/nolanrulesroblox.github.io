9beb3938b9039081b612f200781f5b0839de7bf0
Nolan <54533608+Nolanrulesroblox@users.noreply.github.com>
2021-01-20 12:58:14 +0000

    Flyer, Demo 2

--- src/flyer/flyer-conf.js
+++ src/flyer/flyer-conf.js
@@ -1,15 +1,15 @@
 //flyer conf, // the Flyer Project, NRRINC Media (C) 2021, Apache V2 
 localStorage.setItem('flyer-aid','')
 localStorage.setItem('flyer-title','A Word From Our Sponsor: Crispy')
-localStorage.setItem('flyer-link-href','#')
-localStorage.setItem('flyer-frame-src','/tests/4/src/ad.html')
+localStorage.setItem('flyer-link-href','http://exmaple.com')
+localStorage.setItem('flyer-frame-src','') // iframe
 localStorage.setItem('flyer-frame-rp','unsafe-url')
 localStorage.setItem('flyer-desc','your ad desc')
 localStorage.setItem('flyer-video','mkv.mp4')
 localStorage.setItem('flyer-poster','png.jpg')
-localStorage.removeItem('show-frame','1')
-localStorage.removeItem('show-video','1') 
-localStorage.setItem('show-img','1')
-localStorage.setItem('flyer-img-title','title')
+localStorage.removeItem('show-frame','1') // setItem or removeItem
+localStorage.removeItem('show-video','1') // setItem or removeItem
+localStorage.setItem('show-img','1')      // setItem or removeItem
+localStorage.setItem('flyer-img-title','my cool image')
 localStorage.setItem('flyer-img-src','https://via.placeholder.com/500x375')
 localStorage.setItem('flyer-note','Hey! Welcome to Flyer! http://git.nrrinc.net/flyer')
--- src/flyer/flyer.js
+++ src/flyer/flyer.js
@@ -1,58 +1,72 @@
 // the Flyer Project, NRRINC Media (C) 2021, Apache V2 
-$("head").append('<script src="/src/flyer/flyer-conf.js"></script>');
-$("head").append('<link rel="stylesheet" href="/src/flyer/flyer.css">');
+(function() {
+    var conf = document.createElement('script'); conf.type = 'text/javascript';
+    conf.src = '/src/flyer/flyer-conf.js';
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(conf);
+  })();
+(function() {
+    var css = document.createElement('link'); css.rel = 'stylesheet';
+    css.href = '/src/flyer/flyer.css';
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(css);
+  })();
 $(document).ready(function () {
     if ($("#flyer").length) {
         $("#flyer").append('\
         <h2 id="flyer-t">\
-        <a href="#" id="flyer-title" target="#" alt="Link to Sponsor"></a><button style="position: absolute; right: 35px; top: 25px;background-color: Transparent;cursor:pointer;" onclick="rmvflyer()">X</button>\
+           <a href="#" id="flyer-title" target="_top" alt="Link to Sponsor"></a><button style="position: absolute; right: 35px; top: 25px;background-color: Transparent;cursor:pointer;" onclick="rmvflyer()" id="flyer-xbtn">X</button>\
         </h2>\
         <div id="flyer-content">\
         <img class="flyer-image" id="flyer-image" src="" alt="none" title="" style="display:none;">\
-        <iframe id="flyer-frame" class="flyer-iframe" src="" allowpaymentrequest="false" name="Flyer" allowfullscreen="false" referrerpolicy="unsafe-url"></iframe>\
+        <iframe id="flyer-frame" class="flyer-iframe" src="" allowpaymentrequest="false" name="Flyer" allowfullscreen="false" referrerpolicy="unsafe-url" stlye="display: none;"></iframe>\
+        <video oncontextmenu="return false;" id="flyer-video" width="100%" height="100%" muted style="display:block;" onended="videoEnded()" autoplay="autoplay">\
+        <source src="https://file-examples-com.github.io/uploads/2017/04/file_example_MP4_640_3MG.mp4#t=25" type="video/mp4">\
+        </video>\
         <p class="flyer-desc" id="flyer-desc" alt="ad desc."></p>\
         </div>');
-        // removed, goes above p.flyer-desc         <div class="myVideo" id="flyer_video-m" data-video="#" data-poster="#" data-type="video/mp4"></div>
         $(document).ready(function () {
-            // device locked items
-                
                 $("#flyer-frame").attr('src', localStorage.getItem("flyer-frame-src")); // link to html, htm or img.
                 $("#flyer-frame").attr('sandbox', ''); // edit if needed, to disable iframe scripts.
                 $("#flyer-frame").attr('name', 'Flyer'); // name of iframe ALT
                 $("#flyer-frame").attr('allowfullscreen', 'false');  // do not change.
                 $("#flyer-frame").attr('allowpaymentrequest', 'false'); // do not allow payments
                 $("#flyer-frame").attr('referrerpolicy', localStorage.getItem("flyer-frame-rp")); // what kind of URL is it? Be unsafe if you dont know.
-                // Sponsor image
                 $("#flyer-image").attr('src', localStorage.getItem("flyer-img-src")); // img SRC Gif, webm, PNG or jpeg.
                 $("#flyer-image").attr('title', localStorage.getItem("flyer-img-title")); // title of img, alt
                 $("#flyer-image").attr('alt', localStorage.getItem("flyer-img-alt")); // alt of an alt
-                // Sponsor video
                 $("#flyer_video").attr('data-video', localStorage.getItem("flyer-video-data")); //mp4 only
                 $("#flyer_video").attr('data-poster', localStorage.getItem("flyer-video-poster")); // poster "tumbnail/cover"
                 $("#flyer_video").attr('data-type', 'video/mp4'); // data type
-                //other
                 $("#flyer-title").append(localStorage.getItem("flyer-title")); // top name
+                $("#flyer-title").attr('href',localStorage.getItem("flyer-link-href")); 
                 $("#flyer-desc").append('Your ad here'); // Bottom text
                 $("#flyer-desc").attr('alt', 'ad desc.'); // bottom alt text
-                if ("show-img" in localStorage) {
-                    $("#flyer-image").show();
-                        console.log('Flyer: img');
-                    } else {
-                        $("#flyer-image").remove();
+                function flyerdetect() {
+                    if ("show-img" in localStorage) {
+                        $("#flyer-image").show();
+                            console.log('Flyer: img');
+                        } else {
+                            $("#flyer-image").remove();
+                        };
+                    if ("show-frame" in localStorage) {
+                        $("#flyer-frame").show();
+                            console.log('Flyer: frame'); 
+                        } else { 
+                            $("#flyer-frame").remove();
+                        };
+                    if ("show-show" in localStorage) {
+                        $("#flyer-video").show();
+                            console.log('Flyer: frame'); 
+                        } else { 
+                            $("#flyer-video").remove();
                     };
-                if ("show-frame" in localStorage) {
-                    $("#flyer-frame").show();
-                        console.log('Flyer: frame'); 
-                    } else { 
-                        $("#flyer-frame").remove();
-                    };
+                };
+                flyerdetect();
                 console.log("Flyer: Im Locked and Loaded. Bring. it. on.");
         });
     } else {
         console.error('Flyer: div Not found. Flyer Stopped. to fix, add <div id="flyer"></div> do your page.');
     };
 });
-// ad setting ON/OFF
 $(document).ready(function ($) {
     if ($("#flyer").length) {
     var mode = localStorage.getItem('ad');
@@ -77,14 +91,51 @@ $(document).ready(function ($) {
         $("#yesfly").remove();
 };
 });
-//func
 function rmvflyer() {
-    console.log("Flyer: Removed");
-    $("#flyer").empty();
+    $("#flyer-xbtn").prop('disabled', true);
+    $("#flyer-xbtn").slideUp(1500)
+    .queue(function(nxt) { 
+        $(this).fadeOut(250);
+        $(this).remove();
+        nxt();
+    });
+    $("#flyer-t,#flyer-desc,#flyer-image,#flyer-video,#flyer-frame").slideUp(2500)
+    .queue(function(nxt) { 
+        $(this).empty();
+        nxt();
+    });
     $("#flyer").addClass("hide");
     localStorage.setItem('ad', 'hide');
-    $("#flyer").attr('id','flyer-removed')
-    $("#flyer-removed").append('<div style="padding:20px">Flyer will Not open Again. Ad Closed by Flyer.</div>');
-    setTimeout(function(){$("#flyer-removed").remove();console.log("Flyer: Hidden");}, 5000);
-}
-// <div id="flyer"></div>
+    $("#flyer").attr('id','flyer-removed');
+    $("#flyer-removed").append('<div style="padding:20px">-----<br>Flyer Ad Closed by Flyer. Have a Nice day.<br>-----</div>');
+    setTimeout(function(){
+        $("#flyer-removed").fadeOut(2000).queue(function(nxt) {
+            $(this).empty();
+            nxt();
+        });
+        console.warn("Flyer: Removed");
+    }, 5000);
+};
+function videoEnded() {
+    console.log("Flyer: Video done");
+    $('#flyer-video').fadeOut(2500)
+            .queue(function(nxt) { 
+                $(this).remove();
+                nxt();
+            });
+    $('#flyer').fadeOut(3500)
+            .queue(function(nxt) { 
+                $(this).remove();
+                nxt();
+            });
+    $('#flyer-t').fadeOut(2500)
+    .queue(function(nxt) { 
+        $(this).remove();
+        nxt();
+    });
+    $('#flyer-desc').fadeOut(2500)
+    .queue(function(nxt) { 
+        $(this).remove();
+        nxt();
+    });
+}1000;
